1aa88d0bdaec92a18c9affa853034f734c4d92c2
Tom Hume <twhume@gmail.com>
2012-03-04 14:22:07 +0000

    Merged TransactionBean into PortfolioBean to allow for it to invalidate the PortfolioBean cache.

--- StockPortfolio-war/src/java/org/tomhume/webapps/PortfolioBean.java
+++ StockPortfolio-war/src/java/org/tomhume/webapps/PortfolioBean.java
@@ -1,6 +1,10 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Encapsulates all the methods for reading in lists of ShareTransactions, as
+ * well as adding and deleting transactions.
+ * 
+ * Originally I had these as two separate classes; I brought them together when
+ * I realised that I wanted a delete() or save() operation to invalidate the
+ * cache I was maintaining for listing operations.
  */
 package org.tomhume.webapps;
 
@@ -23,6 +27,15 @@ import javax.inject.Named;
 public class PortfolioBean {
     @EJB PortfolioControllerLocal portfolio; 
     @EJB PortfolioCalculatorLocal calculator;
+    
+    /* Fields used for adding a new ShareTransaction */
+    
+    private String company;
+    private String transactionType;
+    private String purchasePrice;
+    private String submit;
+    private String amount;
+    private String id;
     
     /* List where we store the current portfolio values, so that a JSF page
      * can refer to them many times in the course of a single request without
@@ -55,5 +68,94 @@ public class PortfolioBean {
     public List<ShareValue> getValues() {
         if (svList==null) svList = calculator.getPortfolioValues();
         return svList;
+    }
+    
+    private void invalidateCache() {
+        txCache = new TreeMap<String, List<ShareTransaction>>();
+        svList = null;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSubmit() {
+        return submit;
+    }
+
+    public void setSubmit(String submit) {
+        this.submit = submit;
+    }
+    
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+    
+    public String getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(String purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+    
+    public String[] getCompanies() {
+        return ShareTransaction.COMPANIES;
+    }
+    
+    public String delete() {
+        portfolio.delete(new ShareTransaction(Long.parseLong(this.id)));
+        invalidateCache();
+        return "index";
+    }
+    
+    public String save() {
+        double amount = Double.parseDouble(this.amount);
+        if (this.transactionType.equalsIgnoreCase("Sell")) amount = amount * -1;
+
+        ShareTransaction st = new ShareTransaction();
+        st.setAmount(amount);
+        
+        int pricePaid = centsFromDollars(this.purchasePrice);
+        
+        st.setPricePaid(pricePaid);
+        st.setCompany(this.company);
+
+        portfolio.add(st);
+        invalidateCache();
+        return "index";
+    }
+
+    private int centsFromDollars(String p) {
+        if (!p.contains(".")) {
+            return (Integer.parseInt(p)*100);
+        }
+        String[] fields = p.split(".");
+        //TODO add proper error handling here
+        return (Integer.parseInt(fields[0])*100) + Integer.parseInt(fields[1]);
     }
 }

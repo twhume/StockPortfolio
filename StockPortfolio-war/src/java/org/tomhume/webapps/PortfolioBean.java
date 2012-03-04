16d829a6cc2b352cb03653a523f09c6c300f447d
Tom Hume <twhume@gmail.com>
2012-03-04 15:33:44 +0000

    Now all running under HTTPS, port 8181

--- StockPortfolio-war/src/java/org/tomhume/webapps/PortfolioBean.java
+++ StockPortfolio-war/src/java/org/tomhume/webapps/PortfolioBean.java
@@ -8,13 +8,10 @@
  */
 package org.tomhume.webapps;
 
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import javax.ejb.EJB;
-import javax.ejb.Stateless;
-import javax.enterprise.context.Dependent;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
@@ -55,20 +52,50 @@ public class PortfolioBean {
     public PortfolioBean() {
     }
     
+    /**
+     * Fetch a complete log of all ShareTransactions to date, pulling it from the cache if possible.
+     * 
+     * @return 
+     */
+    
     public List<ShareTransaction> getTransactions() {
         if (txCache.get(ALL_COMPANIES_KEY)==null) txCache.put(ALL_COMPANIES_KEY, portfolio.list());
         return txCache.get(ALL_COMPANIES_KEY);
     }
     
+    /**
+     * Fetch a complete log of all ShareTransactions to date for a given company,
+     * pulling it from the cache if necessary.
+     * 
+     * Hmph, can't remember why I wrote this. But it's the kind of thing which might
+     * come in handy one day, so I'll leave it here.
+     * 
+     * @return 
+     */
+
     public List<ShareTransaction> getTransactionsForCompany(String c) {
         if (txCache.get(c)==null) txCache.put(c, portfolio.listForCompany(c));
         return txCache.get(c);
     }
-    
+
+    /**
+     * Fetch a summary report of the current portfolio, including values at the
+     * valuation provided by the ShareBroker service.
+     * 
+     * @return 
+     */
+
     public List<ShareValue> getValues() {
         if (svList==null) svList = calculator.getPortfolioValues();
         return svList;
     }
+    
+    /**
+     * Empty all the caches.
+     * 
+     * @return 
+     */
+
     
     private void invalidateCache() {
         txCache = new TreeMap<String, List<ShareTransaction>>();
@@ -127,12 +154,27 @@ public class PortfolioBean {
         return ShareTransaction.COMPANIES;
     }
     
+    /**
+     * Delete a single ShareTransaction from our records, referenced by its ID
+     * 
+     * @return 
+     */
+
+    
     public String delete() {
         portfolio.delete(new ShareTransaction(Long.parseLong(this.id)));
         invalidateCache();
         return "index";
     }
     
+    /**
+     * Save a single ShareTransaction into the portfolio, the invalidate the cache
+     * so we see it in the lists we get in the getTransactions... and getValues()
+     * methods above.
+     * 
+     * @return 
+     */
+
     public String save() {
         double amount = Double.parseDouble(this.amount);
         if (this.transactionType.equalsIgnoreCase("Sell")) amount = amount * -1;
@@ -147,14 +189,37 @@ public class PortfolioBean {
 
         portfolio.add(st);
         invalidateCache();
+        cleanParameters();
         return "index";
     }
+    
+    /**
+     * When we've saved a new transaction, we want to clean out all the parameters
+     * so that the addition form isn't pre-filled with the details of the transaction
+     * we just created...
+     */
+    
+    private void cleanParameters() {
+        this.amount = "";
+        this.company = "";
+        this.purchasePrice = "";
+        this.id = "";
+        this.transactionType = "";
+    }
+    
+    /**
+     * Takes a string of the form "100" or "100.01" and returns the number of
+     * cents as an integer. So "100" would become "10000" and "100.01" "10001".
+     * @param p
+     * @return 
+     */
 
     private int centsFromDollars(String p) {
         if (!p.contains(".")) {
             return (Integer.parseInt(p)*100);
         }
-        String[] fields = p.split(".");
+        String[] fields = p.split("\\.");
+        System.err.println("Have " + fields.length + " from " +p);
         //TODO add proper error handling here
         return (Integer.parseInt(fields[0])*100) + Integer.parseInt(fields[1]);
     }
